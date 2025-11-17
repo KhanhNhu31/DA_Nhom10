49ed87451f732ef0b83de6308efe5bf0ee0f328a
KhanhNhu31 <trngknhu1031@gmail.com>
2025-11-18 00:36:45 +0700

    Add files via upload

---
+++ Header1.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void hienThiChuongTrinh;
+void quanLyNhanSu;
+void quanLyPhim;
+void quanLySuatChieu;
+void quanLyDatVe;
+
---
+++ Source1.cpp
@@ -0,0 +1,17 @@
+#include "Header.h"
+
+void quanLyNhanSu() {
+	cout << "\n [Quan ly nhan su]";
+}
+
+void quanLyPhim() {
+	cout << "\n [Quan ly phim]";
+}
+
+void quanLySuatChieu() {
+	cout << "\n [Quan ly suat chieu]";
+}
+
+void quanLyDatVe() {
+	cout << "\n [Dat ve]";
+}
